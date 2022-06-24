ee2773b9649392677001e14956233fcf3803a5d8
santoshgsk <gsk.krishna@gmail.com>
2022-06-24 12:23:47 +0530

    removed ad data fetch code

--- fbads_analysis.py
+++ fbads_analysis.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from datetime import datetime, timedelta
+import pandas as pd
+from fetch_data import get_full_data, increment_date_by_day
 
 
 ACCESS_TOKEN = st.text_input("ACCESS TOKEN")
@@ -6,31 +9,26 @@ AD_ACCOUNT_ID = st.text_input("AD_ACCOUNT_ID")
 
 st.write("now let s see if it is working")
 
-from datetime import datetime, timedelta
-import pandas as pd
-from fetch_data import get_full_data, increment_date_by_day
 
 
-# AD_ACCOUNT_ID = 666100733979996
 
+# all_days_data = None
 
-all_days_data = None
+# date_start = '2022-06-16'
+# date_stop = date_start
 
-date_start = '2022-06-16'
-date_stop = date_start
-
-while date_stop != '2022-6-24':
+# while date_stop != '2022-6-24':
     
-    print ("Processing ... ", date_start)
+#     print ("Processing ... ", date_start)
     
-    start = datetime.now()
-    full_data = get_full_data(date_start, date_stop, AD_ACCOUNT_ID, ACCESS_TOKEN)
-    print (".... 1. Completed data fetching in ", datetime.now()-start)
+#     start = datetime.now()
+#     full_data = get_full_data(date_start, date_stop, AD_ACCOUNT_ID, ACCESS_TOKEN)
+#     print (".... 1. Completed data fetching in ", datetime.now()-start)
     
-    if all_days_data is None:
-        all_days_data = full_data
-    else:
-        all_days_data = pd.concat([all_days_data, full_data], axis=0)
-    date_start, date_stop = increment_date_by_day(date_start, date_stop)
+#     if all_days_data is None:
+#         all_days_data = full_data
+#     else:
+#         all_days_data = pd.concat([all_days_data, full_data], axis=0)
+#     date_start, date_stop = increment_date_by_day(date_start, date_stop)
 
-print (all_days_data)
+# print (all_days_data)
